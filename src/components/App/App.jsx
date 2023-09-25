a060a74a6ff589b03a435945155db7f34b653f9a
Melissa Armstrong <mkarmstr@gmail.com>
2023-09-24 23:10:27 -0400

    First commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import Game from '../Game';
+import Header from '../Header';
+import './App.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+
+      <div className="game-wrapper">
+        <Game />
+      </div>
+    </div>
+  );
+}
+
+export default App;
