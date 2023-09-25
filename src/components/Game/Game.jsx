de59f6a8f4173ea11765536ba5bc8324aa65c509
Melissa Armstrong <mkarmstr@gmail.com>
2023-09-25 02:41:41 -0400

    removed extraneous prev guesses component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import Guess from '../Guess/Guess';
-// import PrevGuesses from '../PrevGuesses/PrevGuesses';
 import GuessGrid from '../GuessGrid/GuessGrid';
 import { checkGuess } from '../../game-helpers';
 import Banners from '../Banners/Banners';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
-// Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
--- src/components/PrevGuesses/index.js
+++
@@ -1,2 +0,0 @@
-export * from './PrevGuesses';
-export { default } from './PrevGuesses';
